67cd5cfb986916755c6f45d6795578f0f0059cb2
Alexandre Lemoine <alexandre.lemoine@isen-ouest.yncrea.fr>
2025-12-16 15:33:09 +0100

    feat(exo5): try 3 tests

--- addition.test.js
+++ addition.test.js
@@ -1,5 +1,13 @@
 const calculerAddition = require('./addition');
 
-test('calculerAddition(15, 5) should return 20', () => {
+test('calculerAddition for caisse 18', () => {
+  expect(calculerAddition(15, 3)).toBe(18);
+});
+
+test('calculerAddition for caisse 20', () => {
   expect(calculerAddition(15, 5)).toBe(20);
+});
+
+test('calculerAddition for caisse 22', () => {
+  expect(calculerAddition(17, 5)).toBe(22);
 });
